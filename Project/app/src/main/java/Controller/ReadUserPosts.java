16536eadad6d28252b2823ca8884caf92cabb4c4
linhlnhs160716 <linhchuoi212@gmail.com>
2025-03-08 21:40:15 +0700

    Post

---
+++ Project/app/src/main/java/Controller/ReadUserPosts.java
@@ -0,0 +1,55 @@
+package Controller;
+import java.sql.ResultSet;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import MODEL.Database;
+
+import MODEL.Post;
+
+import MODEL.User;
+
+public class ReadUserPosts {
+
+
+    private ArrayList<Post> posts;
+
+    public Read UserPosts(User u, Database database) {
+
+        posts = new ArrayList<>();
+
+        String select = "SELECT * FROM `posts WHERE User = " + u.getID() + ";";
+
+        try {
+
+            ResultSet rs = database.getStatement().executeQuery(select);
+
+            while (rs.next()) {
+
+                Post p = new Post();
+
+                p.setID(rs.getInt("ID"));
+
+                p.setContent(rs.getString("Content"));
+
+                p.setDateTimeFromString(rs.getString("DateTime"));
+                p.setUser(u);
+
+                posts.add(p);
+
+            }
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public ArrayList<Post> getPosts() {
+
+        return posts;
+    }
+}
---
+++ Project/app/src/main/java/MODEL/PostCommentAdapter.java
@@ -0,0 +1,50 @@
+package MODEL;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class PostCommentAdapter extends BaseAdapter {
+    private final ArrayList<Post> posts;
+
+    private final ArrayList<Comment> comments;
+    private Activity activity;
+
+    public PostCommentAdapter(ArrayList<Post> posts, ArrayList<Comment> comments,
+
+                              Activity activity) {
+        this.posts = posts;
+        this.comments = comments;
+        this.activity = activity;
+
+    }
+    @Override
+    public int getCount() {
+        return comments.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return comments.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        View v = activity.getLayoutInflater().inflate(R.layout.post_comment, null);
+
+        TextView post_author = v.findViewById(R.id.author);
+
+        return v;
+
+    }
+}
